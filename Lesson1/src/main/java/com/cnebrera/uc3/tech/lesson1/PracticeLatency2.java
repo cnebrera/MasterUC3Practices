238efebfd731f669a41dcafb9d22d64a1b364a94
ais <angel.k.iglesias@gmail.com>
2025-09-02 20:04:15 +0200

    feat: update lesson 1

--- Lesson1/src/main/java/com/cnebrera/uc3/tech/lesson1/PracticeLatency2.java
+++ Lesson1/src/main/java/com/cnebrera/uc3/tech/lesson1/PracticeLatency2.java
@@ -1,13 +1,25 @@
 package com.cnebrera.uc3.tech.lesson1;
 
 import com.cnebrera.uc3.tech.lesson1.simulator.BaseSyncOpSimulator;
-import com.cnebrera.uc3.tech.lesson1.simulator.SyncOpSimulLongOperation;
+import com.cnebrera.uc3.tech.lesson1.simulator.SyncOpSimulSleep;
+import org.HdrHistogram.ConcurrentHistogram;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Second practice, measure latency with and without warmup
+ * Second practice, measure accumulated latency with multiple threads
  */
 public class PracticeLatency2
 {
+    /** Number of consumer threads to run */
+    static final int NUM_THREADS = 4;
+    /** Number of executions per thread */
+    static final int NUM_EXECUTIONS = 100;
+    /** Expected max executions per second */
+    static final int MAX_EXPECTED_EXECUTIONS_PER_SECOND = 500;
+
     /**
      * Main method to run the practice
      * @param args command line arument
@@ -22,28 +34,109 @@ public class PracticeLatency2
      */
     private static void runCalculations()
     {
-        // TODO Create an empty Histogram
+        // TODO Create two different empty concurrent histograms
 
-        // Create a random park time simulator
-        BaseSyncOpSimulator syncOpSimulator = new SyncOpSimulLongOperation();
+        // Create a sleep time simulator, it will sleep for 10 milliseconds in each call
+        BaseSyncOpSimulator syncOpSimulator = new SyncOpSimulSleep(10);
 
-        //Iterate whole cycle for warming up
-        // TODO Increase warm limit to a proper value
-        int warmLimit = 1;
-        for(int k = 0; k < warmLimit; k++) {
+        // List of threads
+        List<Runner> runners = new LinkedList<>();
 
-            // Execute the operation lot of times
-            for (int i = 0; i < 200000; i++) {
-                // TODO Compute latency for each operation
+        // Create the threads and start them
+        for (int i = 0; i < NUM_THREADS; i ++)
+        {
+            // TODO Pass the proper histograms
+            final Runner runner = new Runner(syncOpSimulator, null, null);
+            runners.add(runner);
+            new Thread(runner).start();
+        }
 
+        // Wait for runners to finish
+        runners.forEach(Runner::waitToFinish);
+
+        // TODO Show the percentile distribution of both histograms
+    }
+
+    /**
+     * The runner that represent a thread execution
+     */
+    private static class Runner implements Runnable
+    {
+        /** The shared operation simulator */
+        final BaseSyncOpSimulator syncOpSimulator;
+
+        /** Latency histogram*/
+        private final ConcurrentHistogram histogram;
+
+        /** Accumulated latency histogram*/
+        private final ConcurrentHistogram histogramAcc;
+
+        /** True if finished */
+        volatile boolean finished = false;
+
+        /**
+         * Create a new runner
+         *
+         * @param syncOpSimulator shared operation simulator
+         * @param histogram latency histogram
+         * @param histogramAcc accumulated latency histogram
+         */
+        private Runner(BaseSyncOpSimulator syncOpSimulator, ConcurrentHistogram histogram, ConcurrentHistogram histogramAcc)
+        {
+            this.syncOpSimulator = syncOpSimulator;
+            this.histogram = histogram;
+            this.histogramAcc = histogramAcc;
+        }
+
+        @Override
+        public void run()
+        {
+            // Calculate the expected time between consecutive calls, considering the number of executions per second
+            final long expectedTimeBetweenCalls = TimeUnit.SECONDS.toMillis(1) / MAX_EXPECTED_EXECUTIONS_PER_SECOND;
+
+            // Calculate the next call time, the first time should be immediate
+            long nextCallTime = System.currentTimeMillis();
+
+            // Execute the operation the required number of times
+            for(int i = 0; i < NUM_EXECUTIONS; i++)
+            {
+                // Wait until the next call must be sent
+                while(System.currentTimeMillis() < nextCallTime);
+
+                long startTime = System.currentTimeMillis();
+                // Execute the operation, it will sleep for 10 milliseconds
                 syncOpSimulator.executeOp();
 
+                // TODO Compute latency for each operation in milliseconds
+
+                // TODO Compute delay for each operation in milliseconds
+
                 // TODO Record latency into histogram
+
+                // TODO Compute accumulated latency and record it
+
+                // Calculate the next time to call execute op
+                nextCallTime += expectedTimeBetweenCalls;
             }
 
-            // TODO Show min, max, mean and percentiles 99 and 99,9 with and without warm up (first and last histogram)
+            finished = true;
+        }
 
-            // TODO Reset histogram
+        /** Wait for the runner execution to complete */
+        public void waitToFinish()
+        {
+            while(!this.finished)
+            {
+                try
+                {
+                    Thread.sleep(1);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                    return;
+                }
+            }
         }
     }
 }
